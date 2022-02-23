78640f843ae19e2948ceeede88b3a18a5f5dd8f8
greywind7 <ritik.aggr@gmail.com>
2022-02-23 16:55:22 +0530

    Random forests done

---
+++ 02_regression/05_06_decision_tree_and_random_forest_regression/random_forest_regression.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('Position_Salaries.csv')
+
+x = dataset.iloc[:,1:-1].values
+y = dataset.iloc[:,-1].values
+
+from sklearn.ensemble import RandomForestRegressor 
+regressor = RandomForestRegressor(random_state=0, n_estimators=10)
+
+regressor.fit(x,y)
+print(regressor.predict([[6.5],[7.5]]))
+
+x_grid = np.arange(min(x), max(x), 0.01)
+x_grid = x_grid.reshape((len(x_grid), 1))
+plt.scatter(x,y, color='green')
+plt.plot(x_grid,regressor.predict(x_grid),color='blue')
+plt.title('Salary vs level')
+plt.xlabel('Level')
+plt.ylabel('Salary')
+plt.show()
